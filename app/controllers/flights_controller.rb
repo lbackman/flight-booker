fecfe690f6caece8199dca11fe323329c5c2cd6f
lbackman <lucasbackman@gmail.com>
2023-01-18 15:25:12 +0200

    Build rudimentary flight search
    Still need to implement nested variables like "flight[departure_time]"
    and make use of strong parameters.
    And make the form with form helpers instead of pure html.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.where("departure_time > ?", Time.now).order(:departure_time)
+    @flights = Flight.search(params[:departure_airport_id], params[:arrival_airport_id], params[:departure_time]).upcoming
+    @airports = Airport.all
+    @dates = Flight.get_flight_dates
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,22 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  scope :upcoming, -> { 
+    where("departure_time > ?", Time.now)
+      .order(:departure_time)
+      .includes(:departure_airport, :arrival_airport) 
+    }
+
+  def self.get_flight_dates
+    flight_dates = Flight.upcoming.distinct(:departure_time).pluck(:departure_time)
+  end 
+
+  def self.search(departure_airport, destination_airport, departure_time)
+    if [departure_airport, destination_airport, departure_time].any?
+      where(['departure_airport_id = ? AND arrival_airport_id = ? AND departure_time >= ?', departure_airport, destination_airport, departure_time])
+    else
+      upcoming 
+    end
+  end
 end
