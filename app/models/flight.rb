25676da78a5aa2ebedaee9d3a40611d9c0ba405a
lbackman <lucasbackman@gmail.com>
2023-01-22 12:37:26 +0200

    Change departure_time from datetime to date
    Now the user only sees all unique dated that have flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -9,18 +9,16 @@ class Flight < ApplicationRecord
     }
 
   def self.get_flight_dates
-    flight_dates = Flight.upcoming.distinct(:departure_time).pluck(:departure_time)
+    flight_dates = Flight.where('departure_time > ?', Time.now).distinct.pluck('date(departure_time)').sort
   end 
 
   def self.search(departure_id, arrival_id, departure_time)
-    searches = []
+    searches = [:upcoming]
+
     searches << [:where, 'departure_airport_id = ?', departure_id] if departure_id && departure_id != ''
     searches << [:where, 'arrival_airport_id = ?', arrival_id] if arrival_id && arrival_id != ''
-    if departure_time && departure_time != ''
-      searches << [:where, 'departure_time >= ? AND departure_time < ?', departure_time.to_datetime.midnight, departure_time.to_datetime.midnight + 1.day]
-    else
-      searches << [:upcoming]
-    end
+    searches << [:where, 'date(departure_time) = ?', departure_time] if departure_time && departure_time != ''
+
     searches.inject(Flight) { |flight, where_and_args| flight.send(*where_and_args) }
   end
 end
