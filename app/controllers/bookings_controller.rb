4c6e8b3c6124af428b0ac9063c25e2613a63421d
lbackman <lucasbackman@gmail.com>
2023-01-25 12:56:32 +0200

    Enable creation of bookings and passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,8 +5,22 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to booking_path(@booking.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
+
+  private
+
+    def booking_params
+      params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
+    end
 end
