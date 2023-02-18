18a527321712af2ba26710f499fd7dbcd1b49707
lbackman <lucasbackman@gmail.com>
2023-02-18 17:55:51 +0200

    Update bookings controller and Booking model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -24,7 +24,7 @@ class BookingsController < ApplicationController
   private
 
     def booking_params
-      params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
+      params.require(:booking).permit(:flight_id, passengers_attributes: [:_destroy, :id, :name, :email])
     end
 
     def passenger_amount
