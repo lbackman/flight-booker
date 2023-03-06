f6e5a4f83276383c4b52244b0e248d22c2570ffb
lbackman <lucasbackman@gmail.com>
2023-03-06 16:02:08 +0200

    Set up email preview

--- test/mailers/previews/passenger_mailer_preview.rb
+++ test/mailers/previews/passenger_mailer_preview.rb
@@ -3,7 +3,9 @@ class PassengerMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/passenger_mailer/confirmation_email
   def confirmation_email
-    PassengerMailer.confirmation_email
+    @passenger = Passenger.first
+    @booking = Booking.find(@passenger.booking_id)
+    PassengerMailer.with(booking: @booking).confirmation_email(@passenger)
   end
 
 end
